9558af044762dccf052df3ee039bc1eafa667587
Shorn Jacob <shorn@transparency.consulting>
2019-01-25 14:12:26 +1300

    USed react render . Made a dump component that accpets store and callbacks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
@@ -12,12 +12,32 @@ import {store} from './store'
 // // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-const render = () => document.body.innerText = store.getState()
+// const render = () => document.body.innerText = store.getState()
+
+//dump componnet, displays store, callbacks passed as props are bound to eventhandlers
+const Counter = ({value,onIncrement,onDecrement}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+    
+)
+
+//render fucntion, called from subscribe,renders store in Counter, pass on props as callbacks that calls the actions
+const render = () => {
+    ReactDOM.render(
+       <Counter value={store.getState()}
+       onIncrement={()=> store.dispatch({type : 'INCREMENT'})}
+       onDecrement={()=> store.dispatch({type : 'DECREMENT'})} 
+       />
+        ,document.getElementById('root'))
+}
 
 render()
 
 store.subscribe(render)
 
-document.addEventListener('click', () => {
-    store.dispatch({type : 'INCREMENT'})
-})
+// document.addEventListener('click', () => {
+//     store.dispatch({type : 'INCREMENT'})
+// })
