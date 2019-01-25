c1c7eed5e3133658e2f3b4f6ca9b58234d04c79a
Shorn Jacob <shorn@transparency.consulting>
2019-01-25 13:07:59 +1300

    added increment and decremnt reducers

--- src/reducer/index.test.js
+++ src/reducer/index.test.js
@@ -1,5 +1,21 @@
 import {counter} from './index'
 
-test('counter', ()=> {
-   expect( counter(0, {type: 'INCREMENT'}) ).toBe(0)
-})
+// test('counter', ()=> {
+//    expect( counter(0, {type: 'INCREMENT'}) ).toBe(0)
+// })
+
+test('test increment', ()=> {
+    expect( counter(1, {type: 'INCREMENT'}) ).toBe(2)
+ })
+
+ test('test decrement', ()=> {
+    expect( counter(2, {type: 'DECREMENT'}) ).toBe(1)
+ })
+
+ test('test no initialstate', ()=> {
+    expect( counter(undefined, {type: 'DECREMENT'}) ).toBe(-1)
+ })
+
+ test('test unknow', ()=> {
+    expect( counter(undefined, {type: 'UNKNOWN'}) ).toBe(0)
+ })
