c76088e1c583506e8e95b06bf95d96b8f17fdf31
Shorn Jacob <shorn@transparency.consulting>
2019-01-28 15:36:23 +1300

    Reducer composition, state is object, set visibility

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -46,5 +46,25 @@ const todosReducer = (state = [], action) => {
     }
    }
 
+   const visibilityFilter = ( state = "SHOW_ALL" ,action) => {
+       switch (action.type) {
+           case 'SET_VISIBILITY_FILTER':
+            return action.payload.filter
+            default:
+                return state
+       }
+   }
+
+//when an action comes in ,call a reducer with a part of the state that they manage and the action
+const todoAppReducer = ( state= {}, action) => {
+    return {
+        todos : todos(
+            state.todos,
+            action
+        ),
+        visibilityFilter : visibilityFilter(state.visibilityFilter,action)
+    }
+}
+
 export {todosReducer}
    
