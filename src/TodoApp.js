cc45755cf70b35cfedf460b0dc9dbf9e8ad61c92
Shorn Jacob <shorn@transparency.consulting>
2019-01-29 15:49:15 +1300

    Added a ref input field that adds text for dispatching

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,18 +5,27 @@ import {store} from './store.js'
 let nextTodoId = 200
 
 class TodoApp extends Component {
+
+  constructor(props) {
+    super(props)
+    this.textInput = React.createRef()
+  }
+
   render() {
     return (
       <div>
+        <input ref={this.textInput} />
         <button onClick= { () => {
           store.dispatch ({ 
             type : 'ADD_TODO',
             payload : {
-              text : 'Added',
+              text : this.textInput.current.value,
               id : nextTodoId++
-            }
-           
+            }          
           })
+          console.log( this.textInput.current.value)
+          //reset after dispatching
+          this.textInput.current.value = ''
         }}> Add Todo </button>
         Todo
         <ul>
