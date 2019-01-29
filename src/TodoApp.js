c433134278b988813a4984af6e0fabe542dbc75c
Shorn Jacob <shorn@transparency.consulting>
2019-01-29 16:54:04 +1300

    MAde todo text to have line through it when clicked

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -31,7 +31,18 @@ class TodoApp extends Component {
         <ul>
           {console.log(this.props)}
           {this.props.todos.map(todo => 
-            <li key={todo.id}>
+            <li key={todo.id}
+              //onclick, set style to underline
+                onClick={ () => {
+                  store.dispatch({
+                    type: 'TOGGLE_TODO',
+                    payload: {
+                      id : todo.id
+                    }
+                    
+                  })
+                  //https://www.w3schools.com/jsref/prop_style_textdecoration.asp
+                }} style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
                 {todo.text}
             </li>
             )}
