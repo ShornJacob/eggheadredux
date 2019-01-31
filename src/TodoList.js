ae5e0d0a4b1e25f5570b4d3a40bc35a6ed374742
Shorn Jacob <shorn@transparency.consulting>
2019-01-31 18:28:16 +1300

    Changed list elements to be a seprate jsx

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import Todo from './Todo.js'
 
-export default ({todos,onTodoClick}) => {
-    
+export default ({ todos, onTodoClick }) => {
+
     // console.log(todos)
     // console.log(todos.length)
     // console.log(onTodoClick)
-
-
     
-    return(
-    <ul>
-        {/* Todo returns a <li></li> */}
-        {todos.map ( todo => <Todo 
-                            key={todo.id}
-                            {...todo}
-                            onClick={ ()=> onTodoClick(todo.id)} /> )}
-        
+    //jsx list of todos
+    const litodos = todos.map(todo => <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)} />)
+
+    //console.log(litodos)
+
+    return (
+        <ul>
+           {litodos}
       </ul>
     )
 }
